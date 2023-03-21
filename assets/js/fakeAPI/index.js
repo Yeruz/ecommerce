e612f6f6d7b08c0d2fd015847b06fef34859a64a
yeruz <lipitho13@gmail.com>
2023-03-21 13:13:31 -0600

    Agregando detalles

---
+++ assets/js/fakeAPI/index.js
@@ -0,0 +1,247 @@
+export const lista = [
+  {
+    titulo: "Kimetsu no Yaiba",
+    produtos: [
+      {
+        infos: {
+          id: 1,
+          nome: "Tanjiro Kamado",
+          img: "https://cdn.shopify.com/s/files/1/0604/5761/5547/products/61L8zKLkFFL._AC_SL1500__2_590x.jpg?v=1641950002",
+          preco: 120.0,
+          descricao:
+            "Tanjiro es un adolescente que emprende una búsqueda para restaurar la humanidad de su hermana, Nezuko, quien se convirtió en un demonio después de que su familia fuera asesinada por Muzan Kibutsuji.",
+          alt: "Adolescente con espada",
+        },
+      },
+      {
+        infos: {
+          id: 2,
+          nome: "Inosuke Hashibira",
+          img: "https://cdn.shopify.com/s/files/1/0604/5761/5547/products/610_qHynVLL._AC_SL1500__2_370x.jpg?v=1641949513",
+          preco: 120.02,
+          descricao:
+            "es una persona que aparece utilizando una cabeza de jabalí como máscara a la cual le tiene un gran aprecio, mientras que utiliza un par de espadas todas astilladas que consiguió de una forma moralmente.",
+          alt: "Joven con cabeza de jabalí",
+        },
+      },
+      {
+        infos: {
+          id: 3,
+          nome: "Nezuko Kamado",
+          img: "https://m.media-amazon.com/images/I/61L-JuYpUIL._AC_SY550_.jpg",
+          preco: 120.03,
+          descricao:
+            "Nezuko Kamado es un personaje ficticio del manga Kimetsu no Yaiba de Koyoharu Gotouge. Nezuko y su hermano mayor Tanjirō Kamado son los únicos sobrevivientes de un incidente.",
+          alt: "Señorita con kimono",
+        },
+      },
+      {
+        infos: {
+          id: 4,
+          nome: "Zenitsu Agatsuma",
+          img: "https://cdn.shopify.com/s/files/1/0604/5761/5547/products/61Q7NAAyl9L._AC_SL1500__2_370x.jpg?v=1641950171",
+          preco: 120.04,
+          descricao:
+            " es un Cazador de Demonios y un compañero de viaje de Tanjiro Kamado. A su vez es uno de los personajes principales de Kimetsu no Yaiba.",
+          alt: "Joven con cabello amarillo",
+        },
+      },
+      {
+        infos: {
+          id: 5,
+          nome: "Kyojuro Rengoku",
+          img: "https://cdn.shopify.com/s/files/1/0604/5761/5547/products/51P2Jfxl5tL.jpg?v=1657593621",
+          preco: 120.05,
+          descricao:
+            "Uno de los personajes más famosos de Kimetsu no Yaiba que protagonizó la película Guardianes de la Noche: El tren infinito, junto a Tanjiro, Nezuko y sus compañeros.",
+          alt: "Pilar del Fuego",
+        },
+      },
+      {
+        infos: {
+          id: 6,
+          nome: "Tomioka",
+          img: "https://cdn.shopify.com/s/files/1/0604/5761/5547/products/61cprLpwOSL_370x.jpg?v=1672327804",
+          preco: 120.06,
+          descricao:
+            "Pilar del Agua, es uno de los guerreros más talentosos en Kimetsu no Yiba, debido a que ha sabido derrotar a varios demonios con lo que pudo obtener la posición de Hashira y así superar a su maestro Sakonji Urokodaki.",
+          alt: "Joven reservado con nobles sentimientos",
+        },
+      },
+      {
+        infos: {
+          id: 7,
+          nome: "Uzui",
+          img: "https://cdn.shopify.com/s/files/1/0604/5761/5547/products/6161WMzuKCL_180x.jpg?v=1648225179",
+          preco: 120.07,
+          descricao:
+            "Tengen Uzui es conocido como la Hashira del Sonido de los Nueve Pilares del Cuerpo Demon Slayer, es una persona muy excéntrica que siempre quiere completar acciones extravagantemente como él dice.",
+          alt: "Joven super vanidoso",
+        },
+      },
+    ],
+  },
+  {
+    titulo: "Consola",
+    produtos: [
+      {
+        infos: {
+          id: 1,
+          nome: "Control XBox",
+          img: "https://st4.depositphotos.com/16102236/24811/i/1600/depositphotos_248112922-stock-photo-xbox-gamepad-controller-joystick-blue.jpg",
+          preco: 60.08,
+          descricao:
+            "Imagen 3 de 3 de Mando inalámbrico Microsoft Xbox Serie X|S Mando inalámbrico Joystick blanco Nuevo | 12800 vendido Controlador de joystick inalámbrico Microsoft Xbox Controlador inalámbrico Serie X|S robot blanco",
+          alt: "Control de videojuego",
+        },
+      },
+      {
+        infos: {
+          id: 2,
+          nome: "Fifa 14",
+          img: "https://st2.depositphotos.com/1008960/6132/i/950/depositphotos_61321543-stock-photo-collection-of-fifa-football-games.jpg",
+          preco: 60.09,
+          descricao:
+            "FIFA 14 es un videojuego de simulación de fútbol desarrollado por EA Sports y publicado por Electronic Arts. Fue lanzado en septiembre de 2013 para PlayStation 2, PlayStation 3, PlayStation Portable, PlayStation Vita, XBOX 360, Wii, Nintendo",
+          alt: "cd del juego de futbol",
+        },
+      },
+      {
+        infos: {
+          id: 3,
+          nome: "Control Atari",
+          img: "https://static9.depositphotos.com/1011845/1198/i/950/depositphotos_11985181-stock-photo-joystick.jpg",
+          preco: 60.1,
+          descricao: "Control de video juego antiguo - Retro",
+          alt: "control de video juego antiguo",
+        },
+      },
+      {
+        infos: {
+          id: 4,
+          nome: "Control tipo volante",
+          img: "https://st.depositphotos.com/2702761/3300/i/950/depositphotos_33006217-stock-photo-close-up-of-a-video.jpg",
+          preco: 60.11,
+          descricao:
+            "Comprar Multimedia DVD MP3 Auto Universal Volante Control / Contestar llamadas / Compatible con todas las radios. en Shopee Brasil! ENVÍO INMEDIATO.",
+          alt: "Control en formato de volante",
+        },
+      },
+      {
+        infos: {
+          id: 5,
+          nome: "Playstation 5",
+          img: "https://st2.depositphotos.com/34073418/43117/v/1600/depositphotos_431170678-stock-illustration-illustration-playstation-game-console.jpg",
+          preco: 60.12,
+          descricao:
+            "El juego no tiene límites. Disfruta de una carga ultrarrápida con el SSD de ultra alta velocidad, una inmersión más profunda con soporte de retroalimentación háptica, activadores adaptables y audio 3D, además de una generación sin precedentes de juegos increíbles para la consola Playstation 5 - PS5",
+          alt: "Consola playstation 5",
+        },
+      },
+      {
+        infos: {
+          id: 6,
+          nome: "Fliperama",
+          img: "https://static3.depositphotos.com/1003546/170/v/950/depositphotos_1702441-stock-illustration-retro-games-console.jpg",
+          preco: 60.13,
+          descricao:
+            "Cabina portátil retro coleccionable Street Fighter II Champion Edition Micro Player",
+          alt: "Maquina de juegos retro",
+        },
+      },
+      {
+        infos: {
+          id: 7,
+          nome: "Pac man",
+          img: "https://st.depositphotos.com/2949859/54476/v/1600/depositphotos_544763002-stock-illustration-pac-man-face-arcade-game.jpg",
+          preco: 60.14,
+          descricao:
+            "El personaje y el juego se llamaron Puck-Man, del término japonés paku-paku, que significa que alguien abre y cierra la boca.",
+          alt: "Círculo amarillo con ojo y boca.",
+        },
+      },
+    ],
+  },
+  {
+    titulo: "Otros",
+    produtos: [
+      {
+        infos: {
+          id: 1,
+          nome: "Berserk",
+          img: "https://ramenparados.com/wp-content/uploads/2022/06/berserk-41-jp.jpg",
+          preco: 80.15,
+          descricao:
+            "Berserk es un manga creado por Kentaro Miura y posteriormente adaptado en anime, con un estilo épico fantástico y de fantasía oscura.",
+          alt: "Guerrero solitario y sombrío",
+        },
+      },
+      {
+        infos: {
+          id: 2,
+          nome: "Arcane",
+          img: "https://m.media-amazon.com/images/I/710FQnkOorL.jpg",
+          preco: 80.16,
+          descricao:
+            "Con las dispares ciudades de Piltover y Zaun como telón de fondo, dos hermanas luchan en bandos opuestos de una guerra entre tecnologías mágicas y creencias enfrentadas.",
+          alt: "Jovencita con Pistola",
+        },
+      },
+      {
+        infos: {
+          id: 3,
+          nome: "Nanatsu no Taizai",
+          img: "https://w0.peakpx.com/wallpaper/487/485/HD-wallpaper-meliodas-nanatsu-no-taizai-anime.jpg",
+          preco: 80.17,
+          descricao:
+            "Nanatsu no Taizai (Los siete pecados capitales) es una serie de manga escrita e ilustrada por Nakaba Suzuki. Se serializó en la revista Weekly Shōnen de Kodansha desde octubre de 2012 hasta marzo de 2020, con los capítulos recopilados en cuarenta y un volúmenes de tankōbon.",
+          alt: "Chico rubio con tatuaje en la frente",
+        },
+      },
+      {
+        infos: {
+          id: 4,
+          nome: "Naruto",
+          img: "https://www.formulatv.com/images/series/posters/100/135/dest_1.jpg",
+          preco: 80.18,
+          descricao:
+            "Naruto es una serie de manga escrita e ilustrada por Masashi Kishimoto, que cuenta la historia de Naruto Uzumaki, un joven ninja que busca constantemente el reconocimiento y sueña con convertirse en Hokage, el líder ninja de su aldea.",
+          alt: "Ninja loiro usando bandana",
+        },
+      },
+      {
+        infos: {
+          id: 5,
+          nome: "Dragon Ball",
+          img: "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/35e4ac6339f5fdcc164160a5755790cd.jpe",
+          preco: 80.19,
+          descricao:
+            "Dragon Ball es una franquicia de medios japonesa creada por Akira Toriyama. Originalmente comenzó con una serie de manga que fue escrita e ilustrada por Toriyama, tuvo sus capítulos serializados en la revista Weekly Shonen Jump de 1984 a 1995, con sus 519 capítulos compilados en 42 volúmenes de tankōbon y publicados por la editorial Shueisha.",
+          alt: "hombre de pelo azul",
+        },
+      },
+      {
+        infos: {
+          id: 6,
+          nome: "Demon Slayer",
+          img: "https://planu9.ro/wp-content/uploads/2020/11/Demon-Slayer.jpg",
+          preco: 80.2,
+          descricao:
+            "Kimetsu no Yaiba, también conocida bajo su nombre en inglés Demon Slayer, es una serie de manga escrita e ilustrada por Koyoharu Gotōge, cuya publicación comenzó el 15 de febrero de 2016 en la revista semanal Shūkan Shōnen Jump de la editorial Shūeisha.",
+          alt: "Samurai con espada en mano",
+        },
+      },
+      {
+        infos: {
+          id: 7,
+          nome: "shingeki no kyojin",
+          img: "https://cloudfront-us-east-1.images.arcpublishing.com/elcomercio/7GIIGO2GA5FJBMKXN5XT4KA3NU.jpg",
+          preco: 80.21,
+          descricao:
+            "Los placeres de algunos placeres y similares, la clase de deudas resultará ser ridícula, ¡marrón fácil! ¿Algún matrimonio? Para los miembros, la distinción más pequeña era seguir la orden de la orden. Todos prepararon una rica comida, algo en la playa. ¿El se negó? ¿Convenientemente, porque se decía que los dolores son divertidos? Les doy dolor hasta que complace sus gargantas. La puerta del alma es un alma grande",
+          alt: "hombre con cabello castaño largo",
+        },
+      },
+    ],
+  },
+];
